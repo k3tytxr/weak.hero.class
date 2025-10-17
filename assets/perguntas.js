21a3db60404d6caeb6afba2807a7b23c6ae1d6c7
k3tytxr <ketlyn.teixeira.goncalves@escola.pr.gov.br>
2025-10-17 10:22:17 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "weak hero class ask",
+    "Porque Yeon Si-Eun e Yong Bin tinham rivalidade?",
+    "A rivalidade entre Yeon Si-eun e Jeon Yeong-bin surgiu porque Yeong-bin se sentiu humilhado ao ser superado por Si-eun em uma competição de matemática."
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
