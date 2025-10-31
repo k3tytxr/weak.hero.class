40f416595521e7e8eb5e74ceee8611d5e457055b
k3tytxr <ketlyn.teixeira.goncalves@escola.pr.gov.br>
2025-10-31 10:20:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,12 @@
+criaCartao(
+    "weak hero class ask",
+    "Porque Yeon Si-Eun e Yong Bin tinham rivalidade?",
+    "A rivalidade entre Yeon Si-eun e Jeon Yeong-bin surgiu porque Yeong-bin se sentiu humilhado ao ser superado por Si-eun em uma competição de matemática."
+)
+
+criaCartao(
+    "weak hero class ask",
+    "Por que Oh Beom-seok se sentiu intimidado por young-bin após ele falar de sua antiga escola?",
+    "Porque Oh Beom-seok sofria bullying em sua antiga escola, e Young-bin usou isso ao seu favor"
+)
+
