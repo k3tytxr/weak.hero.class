5920c5a6b018201f43eaa512224212266585545c
k3tytxr <ketlyn.teixeira.goncalves@escola.pr.gov.br>
2025-11-07 10:18:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-function criaCartao (categoria, pergunta, resposta) {
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+function criaCartao(categoria, pergunta, resposta) {
+console.log(categoria, pergunta, resposta);
+let container = document.getElementById('container')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
 
-    cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>weak hero class ask</h3>
-    <div class="cartao__conteudo__pergunta">
-    <p>Por que Yeon Si Eun e Yeong-bin tinham rivalidade? </p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-    <p>A rivalidade entre Yeon Si-eun e Jeon Yeong-bin surgiu porque Yeong-bin se sentiu humilhado ao ser superado por Si-eun em uma competição de matemática.  </p>
-    </div>
-    </div>  
-    `
-    container.appendChild(cartao)
+cartao.innerHTML = `
+<div class='cartao__conteudo'>
+<h3>${categoria}</h3>
+<div class='cartao__conteudo__pergunta'>
+<p>${pergunta}</p>
+</div>
+<div class='cartao__conteudo__resposta'>
+<p>${resposta} </p>
+</div>
+</div>
+`
+container.appendChild(cartao)
 }
